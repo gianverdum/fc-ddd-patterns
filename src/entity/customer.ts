c56e58dd1d676c406f126b4eb7d7eb7fd711e167
gianverdum <gian.verdum@gmail.com>
2025-06-28 10:08:08 -0300

    Princípio de autovalidação

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,20 +2,30 @@ class Customer {
 
     _id: string;
     _name: string;
-    _address: string;
-    _active: boolean = true;
+    _address: string = "";
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
+    }
+
+    validate() {
+        if (!this._id || !this._name) {
+            throw new Error("ID and name are required.");
+        }
     }
 
     changeName(newName: string): void {
         this._name = newName;
+        this.validate();
     }
 
     activate(): void {
+        if (this._address.length === 0) {
+            throw new Error("Cannot activate customer without an address.");
+        }
         this._active = true;
     }
 
