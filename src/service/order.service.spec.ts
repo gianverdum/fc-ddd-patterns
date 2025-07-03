d35e3120f7f1aca83905a3b9cee536475f349fa1
gianverdum <gian.verdum@gmail.com>
2025-07-03 17:06:47 -0300

    Definindo políticas de Reward

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,13 +1,26 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
 
 describe("Order service unit tests", () => {
 
+    it("should place an order", () => {
+        // Arrange
+        const customer = new Customer("c1", "Customer 1");
+        const item1 = new OrderItem("i1", "p1", "Item 1", 100, 1);
+        const item2 = new OrderItem("i2", "p2", "Item 2", 200, 2);
+        // Act
+        const order = OrderService.placeOrder(customer, [item1, item2]);
+        // Assert
+        expect(customer.rewardPoints).toBe(250);
+        expect(order.total()).toBe(500);
+    });
+
     it("should get total of all orders", () => {
         // Arrange
-        const item1 = new OrderItem("i1", "Item 1", "p1", 100, 1);
-        const item2 = new OrderItem("i2", "Item 2", "p2", 200, 2);
+        const item1 = new OrderItem("i1", "p1", "Item 1", 100, 1);
+        const item2 = new OrderItem("i2", "p2", "Item 2", 200, 2);
 
         const order = new Order("o1", "c1", [item1]);
         const order2 = new Order("o2", "c1", [item2]);
